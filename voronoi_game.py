62c8535b6bebadac2a38b4558a70228c95ed2b49
Guyu Fan <guyufan1996@gmail.com>
2017-10-16 21:15:32 -0400

    implement ties in score calculation using an epsilon value of 1e-9

--- voronoi_game.py
+++ voronoi_game.py
@@ -22,6 +22,7 @@ class VoronoiGame:
     self.current_player = 0
     self.moves_made = 0
     self.game_over = False
+    self.epsilon = 1e-9
 
     # server that communicates to client
     self.server = VoronoiServer(host, port, num_players)
@@ -55,10 +56,35 @@ class VoronoiGame:
     else:
       self.server.send(game_info, self.current_player)
 
-  def __send_update_to_node(self, move_row, move_col):
-    data = ''
+  def __generate_compressed_game_bitmap(self):
+    # Compression format is start_index end_index (inclusive) player_owner
+    bitmap = ''
     for score_row in self.score_grid:
-      data += ' '.join(map(str, score_row)) + ' '
+      bitmap += '0 '
+      for i in range(1, len(score_row)):
+        if score_row[i] != score_row[i-1]:
+          # End this compression range and start the next one
+          bitmap += '{} {} {} '.format(str(i - 1), str(score_row[i-1]), str(i))
+      # End the final range
+      bitmap += '{} {} '.format(str(len(score_row) - 1), str(score_row[-1]))
+    return bitmap
+
+  def __generate_decompressed_game_bitmap(self, compressed_bitmap):
+    # This was just written for testing correct (de)compression
+    i = 0
+    compressed_bitmap_array = compressed_bitmap.split(' ')
+    decompressed_bitmap = ''
+    for j in range(0, 10**7, 3):
+      decompressed_bitmap += (compressed_bitmap_array[j + 2] + ' ') * (int(compressed_bitmap_array[j + 1]) - int(compressed_bitmap_array[j]) + 1)
+      if compressed_bitmap_array[j + 1] == '999':
+        i += 1
+        if i == 1000:
+          break
+    return decompressed_bitmap
+
+  def __send_update_to_node(self, move_row, move_col):
+    data = self.__generate_compressed_game_bitmap()
+    # Add rest of meta data
     data += ' '.join(map(str, self.scores)) + ' '
     data += '{} {} '.format(self.num_players, self.current_player + 1)
     data += '{} {}'.format(move_row, move_col)
@@ -104,6 +130,8 @@ class VoronoiGame:
     return int(data[0]), int(data[1])
 
   def __update_scores(self, move_row, move_col):
+    tie_created = 0
+    tie_broken = 0
     # note: score ignores stones, because each player has the same number of stones
     for row in range(self.grid_size):
       for col in range(self.grid_size):
@@ -114,18 +142,42 @@ class VoronoiGame:
         d = self.__compute_distance(row, col, move_row, move_col)
         self.pull[self.current_player][row][col] += float(float(1) / (d*d))
 
-        old_occupier = self.score_grid[row][col]
-        # if the cell has not been claimed by any player
-        if old_occupier == 0:
+        # first move claims every cell on the grid
+        if self.moves_made == 1:
           self.score_grid[row][col] = self.current_player + 1
           self.scores[self.current_player] += 1
-        # if the cell is claimed by some other player
-        elif old_occupier - 1 != self.current_player:
-          # and current player now has a greater pull
-          if self.pull[self.current_player][row][col] > self.pull[old_occupier - 1][row][col]:
-            self.score_grid[row][col] = self.current_player + 1
-            self.scores[old_occupier - 1] -= 1
-            self.scores[self.current_player] += 1
+        # not first move
+        else:
+          old_occupier = self.score_grid[row][col]
+          current_player_pull = self.pull[self.current_player][row][col]
+          # tie: multiple players had equal pull on the cell
+          if old_occupier == 0:
+            # check if current player's pull is now bigger than everyone else's
+            current_player_has_max_pull = True
+            for p in range(self.num_players):
+              if p != self.current_player and self.pull[p][row][col] > current_player_pull - self.epsilon:
+                current_player_has_max_pull = False
+                break
+            # tie broken
+            if (current_player_has_max_pull):
+              self.score_grid[row][col] = self.current_player + 1
+              self.scores[self.current_player] += 1
+              tie_broken += 1
+          # no tie, and cell is claimed by some other player
+          elif old_occupier - 1 != self.current_player:
+            old_occupier_pull = self.pull[old_occupier - 1][row][col]
+            # current player and old player have equal pull, then cell is not owned by anyone
+            if abs(current_player_pull - old_occupier_pull) <= self.epsilon:
+              self.score_grid[row][col] = 0
+              self.scores[old_occupier - 1] -= 1
+              tie_created += 1
+            # current player now has a greater pull, change owner to current player
+            elif current_player_pull > old_occupier_pull:
+              self.score_grid[row][col] = self.current_player + 1
+              self.scores[old_occupier - 1] -= 1
+              self.scores[self.current_player] += 1
+    
+    print("This move broke {} ties and created {} new ties\n".format(tie_broken, tie_created))
 
   def __declare_winner(self):
     max_score = -1
@@ -168,7 +220,7 @@ class VoronoiGame:
 
       # get and validate move
       move_row, move_col = self.__get_player_move()
-      print("{} has placed their stone on: {}, {}\n".format(self.server.names[self.current_player] , move_row, move_col))
+      print("{} has placed their stone on: {}, {}".format(self.server.names[self.current_player] , move_row, move_col))
       if not self.__is_legal_move(move_row, move_col):
         self.scores[self.current_player] = -1
         self.game_over = True

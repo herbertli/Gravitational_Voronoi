96ff1688207f6ba0921be4b7ff43289cc16fa513
Herbert Li <hl1785@nyu.edu>
2018-10-11 19:09:23 -0400

    progress

--- web/src/Board.jsx
+++ web/src/Board.jsx
@@ -1,99 +1,107 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+function decompressBitmap(boardData) {
+  const numericBoard = boardData.split(' ').map(Number);
+  const decompressedBoard = [];
+  let i = 0;
+  for (let j = 0; ; j += 3) {
+    for (let k = numericBoard[j]; k <= numericBoard[j + 1]; k += 1) {
+      decompressedBoard.push(numericBoard[j + 2]);
+    }
+    if (numericBoard[j + 1] === 999) {
+      i += 1;
+      if (i === 1000) {
+        break;
+      }
+    }
+  }
+  return decompressedBoard;
+}
 
 class Board extends React.Component {
+  colors = ['red', 'blue', 'green', 'orange', 'yellow', 'purple', 'silver', 'olive', 'teal'];
 
-    constructor(props) {
-        super(props);
-        this.canvas = React.createRef();
+  colorRGB = [
+    [255, 0, 0],
+    [0, 0, 255],
+    [0, 255, 0],
+    [255, 165, 0],
+    [255, 255, 0],
+    [128, 0, 128],
+    [192, 192, 192],
+    [128, 128, 0],
+    [0, 128, 128],
+  ];
+
+  constructor(props) {
+    super(props);
+    this.canvas = React.createRef();
+  }
+
+  componentDidUpdate() {
+    const { bitmap, moves } = this.props;
+    const ctx = this.canvas.current.getContext('2d');
+    if (!bitmap || bitmap === '' || moves.length === 0) {
+      return;
     }
+    this.drawBoard(ctx, bitmap);
+    this.drawStones(ctx, moves);
+  }
 
-    colors = ['red', 'blue', 'green', 'orange', 'yellow', 'purple', 'silver', 'olive', 'teal'];
-    colorRGB = [
-        [255, 0, 0],
-        [0, 0, 255],
-        [0, 255, 0],
-        [255, 165, 0],
-        [255, 255, 0],
-        [128, 0, 128],
-        [192, 192, 192],
-        [128, 128, 0],
-        [0, 128, 128]
-    ];
+  drawStones(ctx, moves) {
+    for (let b = 0; b < moves.length; b += 1) {
+      for (let a = 0; a < moves[b].length; a += 1) {
+        const centerX = moves[b][a][0];
+        const centerY = moves[b][a][1];
+        const radius = 10;
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+        const color = this.colors[b];
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = '#003300';
+        ctx.stroke();
+      }
+    }
+  }
 
-    decompressBitmap(boardData) {
-        const numericBoard = boardData.split(" ").map(Number);
-        const decompressedBoard = [];
-        let i = 0;
-        for (let j = 0; ; j += 3) {
-            for (let k = numericBoard[j]; k <= numericBoard[j + 1]; k++) {
-                decompressedBoard.push(numericBoard[j + 2]);
-            }
-            if (numericBoard[j + 1] === 999) {
-                i += 1
-                if (i === 1000) {
-                    break;
-                }
-            }
+  drawBoard(ctx, bitmap) {
+    const board = decompressBitmap(bitmap);
+    const imageData = ctx.getImageData(0, 0, 1000, 1000);
+    const { data } = imageData;
+    for (let y = 0; y < 1000; y += 1) {
+      for (let x = 0; x < 1000; x += 1) {
+        const index = (y * 1000 + x) * 4;
+        const player = parseInt(board[index / 4], 10);
+        if (player > 0) {
+          const gridColor = this.colorRGB[player - 1];
+          for (let i = 0; i < 3; i += 1) {
+            data[index + i] = gridColor[i];
+          }
+          data[index + 3] = 125;
         }
-        return decompressedBoard;
+      }
     }
+    ctx.putImageData(imageData, 0, 0);
+  }
 
-    drawStones(ctx, moves) {
-        for (var b = 0; b < moves.length; b++) {
-            for (var a = 0; a < moves[b].length; a++) {
-                var centerX = moves[b][a][0];
-                var centerY = moves[b][a][1];
-                var radius = 10;
-                ctx.beginPath();
-                ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-                var color = this.colors[b];
-                ctx.fillStyle = color;
-                ctx.fill();
-                ctx.lineWidth = 3;
-                ctx.strokeStyle = '#003300';
-                ctx.stroke();
-            }
-        }
-    }
-
-    drawBoard(ctx, bitmap) {
-        var board = this.decompressBitmap(bitmap);
-        var imageData = ctx.getImageData(0, 0, 1000, 1000);
-        var data = imageData.data;
-        for (var y = 0; y < 1000; y++) {
-            for (var x = 0; x < 1000; x++) {
-                var index = (y * 1000 + x) * 4;
-                var player = parseInt(board[index / 4]);
-                if (player > 0) {
-                    var gridColor = this.colorRGB[player - 1];
-                    data[index] = gridColor[0];
-                    data[++index] = gridColor[1];
-                    data[++index] = gridColor[2];
-                    data[++index] = 125;
-                }
-            }
-        }
-        ctx.putImageData(imageData, 0, 0);
-    }
-
-    componentDidUpdate() {
-        let ctx = this.canvas.current.getContext("2d");
-        if (!this.props.bitmap || this.props.bitmap === "" || this.props.moves.length === 0) {
-            return;
-        }
-        this.drawBoard(ctx, this.props.bitmap);
-        this.drawStones(ctx, this.props.moves);
-    }
-
-    render() {
-        return <canvas
-            id={"canvas"}
-            height={1000}
-            width={1000}
-            ref={this.canvas}
-        />
-    }
-
+  render() {
+    return (
+      <canvas
+        id="canvas"
+        height={1000}
+        width={1000}
+        ref={this.canvas}
+      />
+    );
+  }
 }
+
+Board.propTypes = {
+  bitmap: PropTypes.string.isRequired,
+  moves: PropTypes.array.isRequired,
+};
 
 export default Board;
--- web/src/Gameover.jsx
+++ web/src/Gameover.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,55 +8,62 @@ import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
 
 class Gameover extends React.Component {
-
-    colors = ['red', 'blue', 'green', 'orange', 'yellow', 'purple', 'silver', 'olive', 'teal'];
-    colorRGB = [
-        [255, 0, 0],
-        [0, 0, 255],
-        [0, 255, 0],
-        [255, 165, 0],
-        [255, 255, 0],
-        [128, 0, 128],
-        [192, 192, 192],
-        [128, 128, 0],
-        [0, 128, 128]
+  constructor(props) {
+    super(props);
+    this.colors = ['red', 'blue', 'green', 'orange', 'yellow', 'purple', 'silver', 'olive', 'teal'];
+    this.colorRGB = [
+      [255, 0, 0],
+      [0, 0, 255],
+      [0, 255, 0],
+      [255, 165, 0],
+      [255, 255, 0],
+      [128, 0, 128],
+      [192, 192, 192],
+      [128, 128, 0],
+      [0, 128, 128],
     ];
+  }
 
-    render() {
-        let rows = this.props.player_names.map((name, i) => {
-            return (
-                <TableRow key={i}>
-                    <TableCell style={{ background: this.colors[i] }}></TableCell>
-                    <TableCell>{name}</TableCell>
-                    <TableCell>{this.props.total_scores[i]}</TableCell>
-                </TableRow>
-            );
-        });
+  render() {
+    const {
+      player_names,
+      total_scores,
+    } = this.props;
+    const rows = player_names.map((name, i) => (
+      <TableRow key={name}>
+        <TableCell style={{ background: this.colors[i] }} />
+        <TableCell>{name}</TableCell>
+        <TableCell>{total_scores[i]}</TableCell>
+      </TableRow>
+    ));
 
-        return (
-            <div>
-                <Typography variant="h5" gutterBottom style={{ textAlign: "center" }}>
-                    Game Over!
-                    <br />
-                    Final Scores:
-                </Typography>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>#</TableCell>
-                            <TableCell>Player Name</TableCell>
-                            <TableCell>Total Score</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows}
-                    </TableBody>
-                </Table>
-            </div>
-        );
-
-    }
-
+    return (
+      <div>
+        <Typography variant="h5" gutterBottom style={{ textAlign: 'center' }}>
+          Game Over!
+          <br />
+          Final Scores:
+        </Typography>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>#</TableCell>
+              <TableCell>Player Name</TableCell>
+              <TableCell>Total Score</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows}
+          </TableBody>
+        </Table>
+      </div>
+    );
+  }
 }
+
+Gameover.propTypes = {
+  player_names: PropTypes.arrayOf(PropTypes.string).isRequired,
+  total_scores: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
 
 export default Gameover;
